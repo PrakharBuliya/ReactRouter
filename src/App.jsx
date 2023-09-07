ee6a41f419fb554c0a1b7f6a3bd59d1ff04a9ab9
Prakhar Buliya <prakharbuliya02@gmail.com>
2023-09-08 00:15:06 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-green-600 p-4">react router</h1>
+    </div>
+  );
+}
+
+export default App;
